a386e1a6610bb55de5459d0a62ba6fbbea86c31d
Wesley Teixeira <wesleysilvateixeira78@gmail.com>
2026-06-02 22:10:44 -0300

    Update UserRepository.cs

--- StepWise.API/Repositories/UserRepository.cs
+++ StepWise.API/Repositories/UserRepository.cs
@@ -1,19 +1,22 @@
 using MongoDB.Driver;
+using Microsoft.Extensions.Options;
 using StepWise.API.Config;
 using StepWise.API.Models;
-using Microsoft.Extensions.Options;
-using StepWise.API.Repositories;
 
 namespace StepWise.API.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
         private readonly IMongoCollection<User> _users;
 
         public UserRepository(IOptions<MongoDbSettings> settings)
         {
+            if (settings?.Value == null)
+                throw new Exception("MongoDbSettings não carregado");
+
             var client = new MongoClient(settings.Value.ConnectionString);
             var database = client.GetDatabase(settings.Value.DatabaseName);
+
             _users = database.GetCollection<User>("users");
         }
 
@@ -37,27 +40,4 @@ namespace StepWise.API.Repositories
             await _users.ReplaceOneAsync(u => u.Id == id, user);
         }
     }
-}
-public class UserRepository : IUserRepository
-{
-    private readonly IMongoCollection<User> _users;
-
-    public UserRepository(IOptions<MongoDbSettings> settings)
-    {
-        var client = new MongoClient(settings.Value.ConnectionString);
-        var database = client.GetDatabase(settings.Value.DatabaseName);
-        _users = database.GetCollection<User>("users");
-    }
-
-    public async Task<User?> GetByEmailAsync(string email) =>
-        await _users.Find(u => u.Email == email).FirstOrDefaultAsync();
-
-    public async Task CreateAsync(User user) =>
-        await _users.InsertOneAsync(user);
-
-    public async Task<User?> GetByIdAsync(string id) =>
-        await _users.Find(u => u.Id == id).FirstOrDefaultAsync();
-
-    public async Task UpdateAsync(string id, User user) =>
-        await _users.ReplaceOneAsync(u => u.Id == id, user);
 }
